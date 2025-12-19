0d93a651881f5c63f3265503f3e08f6bf2b4337a
Rayen <zarguirayen28@gmail.com>
2025-12-19 13:28:36 +0100

    trigger

--- sklearn_questions.py
+++ sklearn_questions.py
@@ -231,4 +231,3 @@ class MonthlySplit(BaseCrossValidator):
                 elif (date.year, date.month) == test_month:
                     idx_test.append(idx)
             yield (np.array(idx_train), np.array(idx_test))
-            
